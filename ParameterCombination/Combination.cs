feb6f2ec283a0f551f950daa13a88cd4eb5e9bea
KeisukeHoshino <kskhsn15@gmail.com>
2017-02-17 19:28:09 +0900

    create
    create

---
+++ ParameterCombination/Combination.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParameterCombination
+{
+    /// <summary>組み合わせ用の基。IDはなくてもOK</summary>
+    public interface IHasID
+    {
+        int ID { get; }
+    }
+
+    /// <summary>任意のパラメータを持つ用</summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IHasID<T> : IHasID
+    {
+        //IHasIDを継承するのはヒントとして入れてある。
+        T Parameter { get; }
+    }
+
+
+    public static class Combination
+    {
+        /// <summary>
+        /// 2つの配列の合成(!=zip)数え上げ。[a,b],[1,2]  => [(a,1),(a,2),(b,1),(b,2)]
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="values1"></param>
+        /// <param name="values2"></param>
+        /// <returns></returns>
+        public static IEnumerable<IEnumerable<T>> Combine<T>(IEnumerable<T> values1, IEnumerable<T> values2)
+        {
+            return values1.SelectMany(v1 => values2.Select(v2 => new T[] { v1, v2 }));
+        }
+
+        /// <summary>
+        /// 配列の加算みたいの　[(a1,a2),(b1,b2)],[1,2]  => [(a1,a2,1),(a1,a2,2),(b1,b2,1),(b1,b2,2)]
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="values1"></param>
+        /// <param name="values2"></param>
+        /// <returns></returns>
+        public static IEnumerable<IEnumerable<T>> Combine<T>(IEnumerable<IEnumerable<T>> values1, IEnumerable<T> values2)
+        {
+            return values1.SelectMany(v1 => values2.Select(v2 => v1.Concat(new T[] { v2 })));
+        }
+
+
+        /// <summary>
+        /// 全部同じ型ならこれでもよい
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public static IEnumerable<IEnumerable<IHasID>> CreateValuesFromRaw<T>(params IEnumerable<T>[] values)
+        {
+            return CreateValuesCore(values.Select(v => HasID<T>.CreateParameters(v)).ToArray());
+        }
+
+        /// <summary>
+        /// 色々な型のパラメータを入れるならIEnumerable<IHasID>型を作ってから入れること
+        /// </summary>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public static IEnumerable<IEnumerable<IHasID>> CreateValues(params IEnumerable<IHasID>[] values)
+        {
+            return CreateValuesCore(values);
+        }
+
+        /// <summary>
+        /// コア部分
+        /// </summary>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        private static IEnumerable<IEnumerable<IHasID>> CreateValuesCore(params IEnumerable<IHasID>[] values)
+        {
+            //上記2つを同じ名前で使えるように別名で作っておく。
+            if (values == null || values.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+            if (values.Length == 1)
+            {
+                return values[0].Select(v => new IHasID[] { v });
+            }
+            else
+            {
+                var temp = Combine(values[0], values[1]);
+
+                foreach (var item in values.Skip(2))
+                    temp = Combine(temp, item);
+
+                return temp;
+            }
+        }
+
+    }
+
+    public static class HasID
+    {
+        /// <summary>
+        /// 型を間違るとnullそれ以外はちゃんとした値。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static T staticGetParameterSafeFromClass<T>(this IHasID value)
+            where T : class
+        {
+            return (value as IHasID<T>)?.Parameter;
+        }
+
+
+        /// <summary>
+        /// 型を間違えると例外出ます。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static T GetParameter<T>(this IHasID value)
+        //where T : struct
+        {
+            return (value as IHasID<T>).Parameter;
+        }
+
+        /// <summary>
+        /// 型を間違るとnullそれ以外はちゃんとした値。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static T? GetParameterSafe<T>(this IHasID value)
+           where T : struct
+        {
+            return (value as IHasID<T>)?.Parameter;
+        }
+
+        /// <summary>
+        /// value.TryGetValue(out x)と使える。この場合<T>を書かなくてもxの型で類推される。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <param name="dest"></param>
+        /// <returns></returns>
+        public static bool TryGetValue<T>(this IHasID value, out T dest)
+        {
+            var temp = value as IHasID<T>;
+            if (temp != null)
+            {
+                dest = temp.Parameter;
+                return true;
+            }
+            else
+            {
+                dest = default(T);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public static IEnumerable<IHasID<T>> CreateParameters<T>(IEnumerable<T> values)
+        {
+            //こいつの存在で
+            // CreateValues(HasID.GetValues(Enumerable.Range(1, 9)))
+            //に<int>とかなしで類推が効くようになる。
+            return HasID<T>.CreateParameters(values);
+        }
+
+        /// <summary>
+        /// <para> enum版のfactory enum以外は例外発生</para>
+        /// <para> こんな感じで使うHasID.GetEnums<T>() </para>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static IEnumerable<IHasID<T>> CreateParametersFromEnum<T>()
+        {
+            return HasID<T>.CreateParametersFromEnum();
+        }
+    }
+
+    public class HasID<T> : IHasID<T>
+    {
+        /// <summary>
+        /// 要はfactory
+        /// </summary>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public static IEnumerable<IHasID<T>> CreateParameters(IEnumerable<T> values)
+        {
+            return values.Select((v, i) => new HasID<T>(i, v));
+        }
+
+        /// <summary>
+        /// <para> enum版のfactory enum以外は例外発生</para>
+        /// <para> こんな感じで使うHasID<T>.GetEnums() </para>
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<IHasID<T>> CreateParametersFromEnum()
+        {
+            var type = typeof(T);
+            if (!type.IsEnum)
+                throw new ArgumentException();
+
+            return Enum.GetValues(typeof(T)).Cast<T>().Select((c, i) => new HasID<T>(i, c));
+        }
+
+        private HasID(int id, T param)
+        {
+            //常にCreateParameterを使って作ればいいのでprivate
+            this.ID = id;
+            this.Parameter = param;
+        }
+
+        /// <summary>ID</summary>
+        public int ID { get; }
+
+        /// <summary>Parameter</summary>
+        public T Parameter { get; }
+
+
+        public override string ToString() => $"{this.Parameter}({this.ID})";
+
+
+    }
+
+
+    public static class ParameterHelper
+    {
+        public static IEnumerable<Parameters> ToParameters(this IEnumerable<IEnumerable<IHasID>> values) => values.Select(v => new Parameters(v));
+        public static IEnumerable<ParametersI> ToParametersI(this IEnumerable<IEnumerable<IHasID>> values) => values.Select(v => new ParametersI(v));
+
+    }
+
+    public class Parameters
+    {
+        public Parameters(IEnumerable<IHasID> values)
+        {
+            this.values = values.ToArray();
+            this.Count = this.values.Count();
+        }
+
+        public int Count { get; }
+
+        private IHasID[] values;
+
+        public T GetValue<T>(int index)
+        {
+            if (-1 < index && index < this.Count)
+                return HasID.GetParameter<T>(values[index]);
+            else
+                throw new ArgumentOutOfRangeException();
+        }
+
+        public override string ToString() => $"count={this.Count}:[{string.Join<IHasID>(",", this.values)}]";
+    }
+
+    public class ParametersI
+    {
+        public ParametersI(IEnumerable<IHasID> values, int count)
+        {
+            this.values = values;
+            this.Count = count;
+        }
+
+        public ParametersI(IEnumerable<IHasID> values)
+            :this(values,values.Count())
+        {
+        }
+
+
+        public int Count { get; }
+
+        private IEnumerable<IHasID> values;
+
+        public T GetValue<T>(int index)
+        {
+            if (-1 < index && index < this.Count)
+                return HasID.GetParameter<T>(values.ElementAt(index));
+            else
+                throw new ArgumentOutOfRangeException();
+        }
+        public override string ToString() => $"count={this.Count}:[{string.Join<IHasID>(",", this.values)}]";
+
+    }
+}
---
+++ ParameterCombination/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParameterCombination
+{
+    class Program
+    {
+
+        enum AType { A1,A2,A3}
+        enum BType { B1, B2, B3 }
+        enum CType { C1, C2, C3, C4, C5 }
+
+        static void Sample1(AType type1,BType type2,CType type3)
+        {
+            Console.WriteLine($"{type1} , {type2} , {type3} ");
+        }
+
+        static void Sample2(int val1,int val2)
+        {
+            Console.WriteLine($"{val1} * {val2} = {val1 * val2} ");
+        }
+        static void Sample3(int val1, int val2, AType type1, BType type2)
+        {
+
+            Console.WriteLine($"{val1} * {val2} = {val1 * val2} : ({type1},{type2})");
+        }
+
+        static void Main(string[] args)
+        {
+            //case enum
+            var pa1 = HasID.CreateParametersFromEnum<AType>();
+            var pa2 = HasID.CreateParametersFromEnum<BType>();
+            var pa3 = HasID.CreateParametersFromEnum<CType>();
+
+            var paramsA = Combination.CreateValues(pa1, pa2, pa3);
+
+            foreach(var p in paramsA)
+            {
+                var ps = new Parameters(p);
+                Sample1(ps.GetValue<AType>(0), ps.GetValue<BType>(1), ps.GetValue<CType>(2));
+                //var temp = p.ToArray();
+                //Sample1(temp[0].GetParameter<AType>(), temp[1].GetParameter<BType>(), temp[2].GetParameter<CType>());
+            }
+            Console.WriteLine($"paramsA count = {paramsA.Count()}");// 3 * 3 * 5  = 45
+
+            //case int[] part1
+            var pb1 = HasID.CreateParameters(Enumerable.Range(5, 10).ToArray());
+            var pb2 = HasID.CreateParameters(new int[] { 99, 999, 9999 });
+
+            var paramsB = Combination.CreateValues(pb1, pb2);
+          
+            foreach (var p in paramsB)
+            {
+                var ps = new Parameters(p);
+                Sample2(ps.GetValue<int>(0), ps.GetValue<int>(1));
+                //var a = p.ToArray();
+                //Sample2(a[0].GetParameter<int>(), a[1].GetParameter<int>());
+            }
+            Console.WriteLine($"paramsB count = {paramsB.Count()}");
+
+            //case int[] part2
+            var pc1 = Enumerable.Range(5, 10);
+            var pc2 = new int[] { 99, 999, 9999 };
+
+            var paramsC = Combination.CreateValuesFromRaw(pc1, pc2);
+
+            foreach (var p in paramsC)
+            {
+                var ps = new Parameters(p);
+                Sample2(ps.GetValue<int>(0), ps.GetValue<int>(1));
+                //var temp = p.ToArray();
+                //Sample2(temp[0].GetParameter<int>(), temp[1].GetParameter<int>());
+            }
+            Console.WriteLine($"paramsC count = {paramsC.Count()}");
+
+            //mix
+            var paramsD = Combination.CreateValues(pb1, pb2,pa1,pa2);
+
+            foreach (var p in paramsD)
+            {
+                var ps = new Parameters(p);
+                Sample3(ps.GetValue<int>(0), ps.GetValue<int>(1), ps.GetValue<AType>(2), ps.GetValue<BType>(3));                
+            }
+            Console.WriteLine($"paramsD count = {paramsD.Count()}");
+
+
+            //
+            var paramsE = Combination.CreateValues(pb1, pb2, pa1, pa2);
+            var l1 = paramsE.Select(v => new ParametersI(v, 4)).Where(v => v.GetValue<int>(0) * v.GetValue<int>(1) > 1000).ToArray();
+
+            var l2 = paramsE.ToParameters();
+            var l3 = paramsE.ToParameters().Where(v => v.GetValue<int>(0) * v.GetValue<int>(1) > 1000).ToArray();
+
+           foreach(var p in l3)
+            {
+                Console.WriteLine(p);
+            }
+
+        }
+    }
+}
